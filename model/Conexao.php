9d2febc541b1719c19a193ac6d270683c65d2b1c
Gustavo de Souza Costa Joia <99343256+GustavoJoia@users.noreply.github.com>
2023-04-26 22:38:48 -0300

    Update Conexao.php

--- model/Conexao.php
+++ model/Conexao.php
@@ -3,7 +3,7 @@
     {
         public static function conectar()
         {
-            $conexao = new PDO('mysql:host=fdb30.awardspace.net;dbname=3763351_crud;charset=utf8', '3763351_crud', 'pwfinal123');
+            $conexao = new PDO('mysql:host=fdb30.awardspace.net;dbname=3763351_crud;charset=utf8', '3763351_crud', '');
             //$conexao = new PDO('mysql:host=localhost;dbname=3763351_crud;charset=utf8', 'root', '');
             
             $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
